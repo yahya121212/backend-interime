1abeef05a9a5bcc61a4cb9876e6228728bd18590
YAHYA <yahya.benhajomor@gmail.col>
2025-09-10 15:18:31 +0100

    ref

--- src/experience/entities/experience.entity.ts
+++ src/experience/entities/experience.entity.ts
@@ -21,6 +21,9 @@ export class Experience {
   @Column({ nullable: true, type: 'text' })
   description?: string; // Description of duties or achievements in the position
 
-  @ManyToOne(() => Candidate, (candidate) => candidate.experiences)
-  candidate: Candidate;
+@ManyToOne(() => Candidate, (candidate) => candidate.experiences, {
+  onDelete: 'SET NULL',
+  nullable: true,
+})
+candidate: Candidate;
 }
