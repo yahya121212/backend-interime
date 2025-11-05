c334f6e741d3e1c1677aa6c73d7814d8fcf34baa
YAHYA <yahya.benhajomor@gmail.col>
2025-11-06 00:03:34 +0100

    ref

--- src/location/location.module.ts
+++ src/location/location.module.ts
@@ -5,11 +5,23 @@ import { Location } from './entities/location.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostalCodeModule } from 'src/postal-code/postal-code.module';
 import { CityModule } from 'src/city/city.module';
+import { Region } from 'src/region/entities/region.entity';
+import { Department } from 'src/department/entities/department.entity';
+import { City } from 'src/city/entities/city.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Location]), PostalCodeModule, CityModule],
+  imports: [
+    TypeOrmModule.forFeature([
+      Location,
+      Region,        // ✅ ajouté
+      Department,    // ✅ ajouté
+      City,          // ✅ ajouté
+    ]),
+    PostalCodeModule,
+    CityModule,
+  ],
   controllers: [LocationController],
   providers: [LocationService],
   exports: [LocationService],
 })
-export class LocationModule {}
+export class LocationModule { }
